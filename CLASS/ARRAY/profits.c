baabffa7e2d153913f70ad3af979d5b71713e36d
ISHAAN VAISH <224352959+ishaanvaish06@users.noreply.github.com>
2026-03-09 10:45:03 +0530

    Create profits.c

---
+++ CLASS/ARRAY/profits.c
@@ -0,0 +1,27 @@
+
+//ch.sc.u4aie25040
+//profit case
+#include <stdio.h>
+
+int main() {
+    int t,i;
+    scanf("%d", &t);
+    while(t--) {
+        int n;
+        scanf("%d", &n);
+        int arr[n];
+        for(i=0;i<n;i++)
+            scanf("%d",&arr[i]);
+        int profit=0;
+        for(i=1;i<n;i++) {
+            if(arr[i]>arr[i-1]) {
+                printf("(%d %d) ",i-1,i);
+                profit=1;
+            }
+        }
+        if(profit==0)
+            printf("No Profit");
+        printf("\n");
+    }
+    return 0;
+}
