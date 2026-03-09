ab393fa17ebe95e02f2fa812f8cc07b6982f68d2
ISHAAN VAISH <224352959+ishaanvaish06@users.noreply.github.com>
2026-03-09 10:52:55 +0530

    Create two.c

---
+++ CLASS/two.c
@@ -0,0 +1,52 @@
+//CH.SC.U4AIE25020
+#include<stdio.h>
+#include<stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+void sortedInsert(struct Node **head_ref,struct Node *new_node)
+{
+    struct Node *current;
+    if(*head_ref==NULL || (*head_ref)->data>=new_node->data)
+    {
+        new_node->next=*head_ref;
+        *head_ref=new_node;
+    }
+    else
+    {
+        current=*head_ref;
+        while(current->next!=NULL && current->next->data<new_node->data)
+            current=current->next;
+        new_node->next=current->next;
+        current->next=new_node;
+    }
+}
+void display(struct Node *head)
+{
+    struct Node *temp=head;
+    while(temp!=NULL)
+    {
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+    printf("\n");
+}
+int main()
+{
+    int n,i,x;
+    struct Node *head=NULL;
+    struct Node *newNode;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&x);
+        newNode=(struct Node*)malloc(sizeof(struct Node));
+        newNode->data=x;
+        newNode->next=NULL;
+        sortedInsert(&head,newNode);
+    }
+    display(head);
+    return 0;
+}
